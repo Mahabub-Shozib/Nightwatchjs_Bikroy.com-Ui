bd65680f4ee2a00168ec2709a43a3b2304e75a2d
Mahabub-Shozib <mahabub.qups@gmail.com>
2021-07-29 16:05:39 +0600

    simple page-objects

---
+++ Utils/locators.js
@@ -0,0 +1,6 @@
+const login = "//div[@class='only-desktop--2pxYG']//span[contains(text(),'Login')]"
+const continueWithMail = "//span[@class = 'email-text--FbASN']"
+const imputEmail = "//input[@id='input_email']"
+const inputPassword = "//input[@id='input_password']"
+const loginButton = "//button[@class='btn--1gFez secondary--Os-e9 background--2lR9B small--1MQ15 gtm-email-login disabled--1OIBt']"
+const myAccount = "//span[contains(text(),'My account')]"
